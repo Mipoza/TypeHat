c524aa77e1dfe50bbc5156095fae3ed4dd7272e0
vincent boulard <vboulard07@gmail.com>
2020-07-24 19:22:03 +0200

    added readme

---
+++ manager.py
@@ -0,0 +1,28 @@
+import snet
+import threading
+from cryptography.fernet import Fernet
+
+serv = snet.ss_serv(2012)
+
+def listenning():
+    serv.listen(10)
+    while True:
+        serv.wait_connect()
+
+
+if __name__ == "__main__":
+    threading.Thread(target=listenning).start()
+    
+
+
+
+"""
+key = Fernet.generate_key()
+
+s = snet.secure_socket(key)
+
+try:
+    print(s.t)
+except AttributeError as a:
+    print(a)
+"""
---
+++ snet.py
@@ -0,0 +1,58 @@
+import socket
+from cryptography.fernet import Fernet
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+class secure_socket:
+    def __init__(self, key, socket, buffer=2048):
+        self.key = key
+        self.encrypter = Fernet(key)
+        self.socket = socket
+        self.buffer = buffer
+    
+    def secure_send(self, data):
+        to_send = data
+        if(type(data) == str):
+            to_send = to_send.encode()
+        try:
+            result = self.socket.send(self.encrypter.encrypt(to_send))
+        except:
+            print("An error occured with send")
+        else:
+            return result
+
+    def secure_recv(self):
+        try:
+            result = self.encrypter.decrypt(self.socket.recv(self.buffer))
+        except:
+            print("An error occured with send")
+        else:
+            return result
+
+class ss_serv():
+    connection_list = []
+    def __init__(self, port, buffer=2048):
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.buffer = buffer
+        self.socket.bind(('',self.port))
+        self.key_RSA = RSA.generate(2048)
+    
+    def listen(self, max):
+        self.socket.listen(max)
+    
+    def wait_connect(self):
+        try:
+            print("dd")
+            connection, infos = self.socket.accept()
+            print("ddddd")
+            connection.send(self.key_RSA.publickey().export_key())
+            enc_key = connection.recv(self.buffer)
+
+            cipher_rsa = PKCS1_OAEP.new(self.key_RSA)
+            self.key = cipher_rsa.decrypt(enc_key)
+            ss = secure_socket(self.key, connection, self.buffer)
+
+            connection_list.append(ss)
+        except socket.error as err:
+            print("error occured for accepting socket : {}".format(err))
